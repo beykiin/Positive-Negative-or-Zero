bc437a4f81f87abe1b991a6d9900a1b923ddcd0d
beykiin <132366582+beykiin@users.noreply.github.com>
2024-02-18 14:20:28 +0300

    Add files via upload

---
+++ Positive, Negative, or Zero.cpp
@@ -0,0 +1,41 @@
+﻿// Positive, Negative, or Zero.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//Checking If Number Entered From User Is Positive, Negative, or Zero
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if (num > 0) {
+
+        cout << "The number entered is positive." << endl;
+
+    }
+    else if (num < 0) {
+
+        cout << "The number entered is negative." << endl;
+
+    }else {
+
+        cout << "The number entered is zero." << endl;
+
+    }
+
+    return 0;
+
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
